2e2055cc84a7a7eb324bae123e05205a5756b4f9
itbeard <iamitbeard@gmail.com>
2025-06-09 23:34:41 +0200

    ♻️ Refactor logging and error messages to use utility function
    Replaced hard-coded handler name strings in logging and error messages with a dynamic utility function call `utils.GetCurrentTypeName()`. This improves maintainability, ensuring that any changes to the handler type name only need to be updated in one place. Added import for the new utility.

--- internal/handlers/grouphandlers/join_left_handler.go
+++ internal/handlers/grouphandlers/join_left_handler.go
@@ -2,6 +2,7 @@ package grouphandlers
 
 import (
 	"evo-bot-go/internal/database/repositories"
+	"evo-bot-go/internal/utils"
 	"fmt"
 	"log"
 
@@ -32,26 +33,26 @@ func (h *JoinLeftHandler) handle(b *gotgbot.Bot, ctx *ext.Context) error {
 	if isNowMember {
 		dbUser, _, err := h.userRepo.GetOrFullCreate(&user)
 		if err != nil {
-			return fmt.Errorf("JoinLeftHandler: failed to get or create user in JoinLeftHandler: %w", err)
+			return fmt.Errorf("%s: failed to get or create user: %w", utils.GetCurrentTypeName(), err)
 		}
-		log.Printf("JoinLeftHandler: User %s (%d) is now a member, setting IsClubMember to true", user.Username, user.Id)
+		log.Printf("%s: User %s (%d) is now a member, setting IsClubMember to true", utils.GetCurrentTypeName(), user.Username, user.Id)
 		err = h.userRepo.SetClubMemberStatus(dbUser.ID, true)
 		if err != nil {
-			return fmt.Errorf("JoinLeftHandler: failed to set club member status to true for user %d: %w", dbUser.ID, err)
+			return fmt.Errorf("%s: failed to set club member status to true for user %d: %w", utils.GetCurrentTypeName(), dbUser.ID, err)
 		}
 
 	} else if isNowLeftOrBanned {
 		dbUser, _, err := h.userRepo.GetOrFullCreate(&user)
 		if err != nil {
-			log.Printf("JoinLeftHandler: User %d who is now left/banned not found in DB, nothing to update: %v", user.Id, err)
-			return nil // Not an error, user might have never interacted with the bot
+			return fmt.Errorf("%s: failed to get or create user: %w", utils.GetCurrentTypeName(), err)
+
 		}
 
 		if dbUser.IsClubMember {
-			log.Printf("JoinLeftHandler: User %s (%d) is now left/banned, setting IsClubMember to false", user.Username, user.Id)
+			log.Printf("%s: User %s (%d) is now left/banned, setting IsClubMember to false", utils.GetCurrentTypeName(), user.Username, user.Id)
 			err := h.userRepo.SetClubMemberStatus(dbUser.ID, false)
 			if err != nil {
-				return fmt.Errorf("JoinLeftHandler: failed to set club member status to false for user %d: %w", dbUser.ID, err)
+				return fmt.Errorf("%s: failed to set club member status to false for user %d: %w", utils.GetCurrentTypeName(), dbUser.ID, err)
 			}
 		}
 	}
